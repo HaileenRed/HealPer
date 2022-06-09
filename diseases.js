f95bfc256b3511b37f14dbff05caaab5adf28d10
HaileenRed <106913091+HaileenRed@users.noreply.github.com>
2022-06-09 21:21:36 +0800

    Update diseases.js

--- diseases.js
+++ diseases.js
@@ -1,59 +1,96 @@
 let diseases = [
-  {
-    Disease: 'Allergies',
-    Link: 'https://www.mayoclinic.org/diseases-conditions/allergies/symptoms-causes/syc-20351497',
-    Symtomps: [
-      'eye irritation',
-      'runny nose',
-      'stuffy nose',
-      'puffy, watery eyes',
-      'sneezing',
-      'inflamed, itchy nose and throat',
-    ],
-  },
-  {
-    Disease: 'Cold and Flu',
-    Link: 'https://www.webmd.com/cold-and-flu/default.htm',
-    Symtomps: [
-      'fever (100° F)',
-      'headache',
-      'more intense pain and fatigue',
-      'more severe, often dry cough',
-    ],
-  },
-  {
-    Disease: 'Conjunctivitis (“pink eye“)',
-    Link: 'https://www.webmd.com/eye-health/eye-health-conjunctivitis',
-    Symtomps: [
-      'redness, itching, tearing, burning sensation',
-      'pus-like discharge and/or crusting of the eyelids',
-    ],
-  },
-  {
-    Disease: 'Diarrhea',
-    Link: 'https://www.webmd.com/digestive-disorders/digestive-diseases-diarrhea',
-    Symtomps: [
-      'watery, loose stools',
-      'frequent bowel movements',
-      'cramping or pain in the abdomen, nausea, bloating',
-      'possibly fever or bloody stools, depending on the cause',
-    ],
-  },
-  {
-    Disease: 'Headaches',
-    Link: 'https://www.webmd.com/migraines-headaches/migraines-headaches-basics',
-    Symtomps: ['symptoms of a migraine', 'symptoms of a tension-type headache'],
-  },
-  {
-    Disease: 'Mononucleosis',
-    Link: 'https://www.mayoclinic.org/diseases-conditions/mononucleosis/symptoms-causes/syc-20350328',
-    Symtomps: [
-      'nap frequently',
-      'swollen lymph glands and spleen',
-      'sore throat',
-      'fever',
-      'loss of appetite',
-      'muscle aches',
-    ],
-  },
-]
+   {
+      Disease: 'Allergy',
+      Link: 'https://en.wikipedia.org/wiki/Allergy'
+      Symtomps: [
+         'eye irritation',
+         'runny nose',
+         'stuffy nose',
+         'puffy, watery eyes',
+         'sneezing',
+         'inflamed, itchy nose and throat', 
+      ],
+    },
+    {
+      Disease: 'Abdominal Aortic Aneurysm',
+      Link: 'https://en.wikipedia.org/wiki/Abdominal_aortic_aneurysm',
+      Symtomps: [
+        'back or flank pain',
+        'severe or worsening abdominal pain',
+        'feeling a pulse near the bellybutton',
+      ],
+    },
+    {
+      Disease: 'Cold and Flu',
+      Link: 'https://en.wikipedia.org/wiki/Common_cold',
+      Symtomps: [
+         'fever',
+        'headache',
+         'more intense pain and fatigue',
+        'more severe, often dry cough',
+       ],
+    }, 
+     {
+      Disease: 'Alkaptonuria',
+      Link: 'https://en.wikipedia.org/wiki/Alkaptonuria',
+      Symtomps: [
+        'dark spots in the sclera (white) of your eyes',
+        'thickened and darkened cartilage in your ears',
+        'blue speckled discoloration of your skin, particularly around sweat glands',
+        'dark-colored sweat or sweat stains',
+        'black earwax',
+      ],
+    },
+    {
+      Disease: 'Aortic stenosis',
+      Link: 'https://en.wikipedia.org/wiki/Aortic_stenosis',
+      Symtomps: [
+        'Chest pain',
+        'Rapid or fluttering heartbeat',
+        'Trouble breathing or feeling short of breath',
+        'Feeling dizzy or light-headed and even fainting',
+        'Difficulty walking short distances',
+        'Swollen ankles or feet',
+        'Difficulty sleeping or needing to sleep sitting up'
+      ],
+    },
+    {
+      Disease: 'Conjunctivitis (“pink eye“)',
+      Link: 'https://en.wikipedia.org/wiki/Conjunctivitis#Viral',
+      Symtomps: [
+        'redness, itching, tearing, burning sensation in the eyes',
+        'pus-like discharge and/or crusting of the eyelids',
+      ],
+    },
+    {
+      Disease: 'Diarrhea',
+      Link: 'https://en.wikipedia.org/wiki/Diarrhea',
+      Symtomps: [
+        'watery, loose stools',
+        'frequent bowel movements',
+        'cramping or pain in the abdomen, nausea, bloating',
+        'feeling you need to vomit',
+        'possibly fever or bloody stools, depending on the cause',
+      ],
+    },
+    {
+      Disease: 'Migraine',
+      Link: 'https://en.wikipedia.org/wiki/Migraine',
+      Symtomps: ['sensitive to light or sound', 
+                 'feeling you need to vomit',
+      ],
+    },
+    {
+      Disease: 'Mononucleosis',
+      Link: 'hhttps://en.wikipedia.org/wiki/Infectious_mononucleosis',
+      Symtomps: [
+        'nap frequently',
+        'swollen lymph glands and spleen',
+        'sore throat',
+        'fever',
+        'loss of appetite',
+        'muscle aches',
+      ],
+    },
+  ]
+  
