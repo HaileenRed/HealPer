2c10593e7caa2e0d302879bd0c5bbcbd8aaed6e4
HaileenRed <106913091+HaileenRed@users.noreply.github.com>
2022-06-10 12:49:33 +0800

    Update readmore.js

--- readmore.js
+++ readmore.js
@@ -23,6 +23,145 @@
 //     ],
 //   },
 //   {
+// Disease: 'Achalasia g',
+// Symtomps: [
+//   'trouble swallowing food (dysphagia)',
+//    'food or liquid flowing back up into the throat (regurgitation)', 
+//    'waking up at night coughing or choking due to regurgitation',
+//    'chest pain',
+//    'trouble burping',
+//    'hiccups', 
+//    'weight loss due to reduced intake of food'.
+//    ],
+//  }, 
+//  { 
+//  Disease: 'Addison Disease (Adrenal Insufficiency)',
+//  Symtomps: [ 
+//  'Depression',
+//  'Diarrhea',
+//  'Fatigue',
+//  'Hyperpigmentation of Skin (Skin Darkening)', 
+//  'Irregular Menstrual Periods',
+//  'Irritability',
+//  'Loss of Appetite',
+//  'Low Blood Pressure',
+//  'Muscle Weakness',
+//  'Feeling like you need to vomit',
+//  'Salt Cravings',
+//  'Vomiting',
+//  'Weight Loss',
+// ],
+//  },
+// {
+//  
+// Disease: 'Alkapeonuria',
+// Symtomps: ['Discoloration of the Earwax',
+//  'Heart Problems',
+//  'Kidney Stones',
+//  'Ochronosis (Bluish-Black Pigmentation in Connective Tissue) * Prostate Stones',
+//  'Urine That Turns Black When Exposed to Air',
+// ],
+// },
+// {
+//  Disease:'Aortic Stenosis',
+//  Symtomps: [
+//   'Abnormal Heart Rhythms',
+//   'Chest Pain',
+//  'Fainting',
+//  'Fatigue',
+//  'Heart Murmur',
+//   'Inability to Exercise',
+//  'Lightheadedness',
+// 'Shortness of Breath',
+//    ],
+// },
+// { 
+//   Disease: 'ARDS (Acute Respiratory Distress Syndrome)',
+//   Symtomps: [
+//   'Confusion',
+//   'Cough',
+//   'Bluish Nails or Lips', 
+//   'Dizziness',
+//   'Exhaustion (Tiredness)',
+//   'Fever',
+//   'Headache',
+//   'Low Blood Pressure',
+//   ],
+//  },
+//  {
+//  Disease: 'Absence seizure',
+//  Symptomps: [
+//  'Sudden stop in motion without falling',
+//  'Lip smacking',
+//  'Eyelid flutters',
+//  'Chewing motions',
+//  'Finger rubbing',
+//  'Small movements of both hands',
+//   ],
+//  },
+//  {
+//  Disease: 'Acanthosis nigricans', 
+//  Symtomps: [
+//  'Skin changes are the only signs of acanthosis nigricans',
+//  'You will notice dark, thickened, velvety skin on body folds and creases', 
+//  'typically in your armpits, groin and back of the neck',
+//   'The affected skin changes usually appear slowly',
+//   'The affected skin may also have an odor or itch',
+//    ],
+//  },
+//  {
+//  Disease: 'Achilles tendinitis', 
+//  Symtomps: [
+//  'The pain associated with Achilles tendinitis typically begins as a mild ache in the back of the leg or above the heel after running or other sports activity',
+//  'Episodes of more-severe pain may occur after prolonged running, stair climbing or sprinting',
+//  'You might also experience tenderness or stiffness, especially in the morning, which usually improves with mild activity',
+//    ],
+//  },
+//  {
+// Disease: 'ADHD (Hyperactive Disorder)',
+// Symtomps: [
+// 'Impulsiveness',
+// 'Disorganization and problems prioritizing. Poor time management skills',
+// 'Problems focusing on a task',
+// 'Trouble multitasking',
+// 'Excessive activity or restlessness',
+// 'Poor planning',
+// 'Low frustration tolerance',
+//    ],
+//  },
+//  {
+// Disease: 'Autism',
+// Symtomps: [
+// 'Abnormal Body Posturing or Facial Expressions. Abnormal Tone of Voice',
+// 'Avoidance of Eye Contact or Poor Eye Contact. Behavioral Disturbances', 
+// 'Deficits in Language Comprehension',
+// 'Delay in Learning to Speak',
+// 'Flat or Monotonous Speech',
+// 'Inappropriate Social Interaction',
+//    ],
+//  },
+//  {
+// Disease: 'Achilles tendon rupture',
+// Symtomps: [
+// 'The feeling of having been kicked in the calf',
+//  'Pain, possibly severe, and swelling near the heel',
+//  'An inability to bend the foot downward or "push off" the injured leg when walking',
+//  'An inability to stand on the toes on the injured leg',
+// 'A popping or snapping sound when the injury occurs',
+//   ],
+//  },
+//  {
+// Disease: 'Acid reflux',
+//  Symtomps: [
+//  'Is not gaining weight',
+//  'Consistently spits up forcefully, causing stomach contents to shoot out of his or her mouth',
+//  'Spits up green or yellow fluid',
+//  'Spits up blood or a material that looks like coffee grounds',
+//   'Refuses food',
+//  'Has blood in his or her stool',
+//  'Has difficulty breathing or a chronic cough',
+// 'Begins spitting up at age 6 months or older',
+// 'Is unusually irritable after eating',
 //      Disease: 'Alkaptonuria',
 //       Symtomps: [
 //      'dark spots in the sclera (white) of your eyes',
@@ -30,18 +169,6 @@
 //      'blue speckled discoloration of your skin, particularly around sweat glands',
 //      'dark-colored sweat or sweat stains',
 //      'black earwax',
-//     ],
-//   },
-//   {
-//      Disease: 'Aortic stenosis',
-//        Symtomps: [
-//      'Chest pain',
-//      'Rapid or fluttering heartbeat',
-//      'Trouble breathing or feeling short of breath',
-//      'Feeling dizzy or light-headed and even fainting',
-//      'Difficulty walking short distances',
-//      'Swollen ankles or feet',
-//      'Difficulty sleeping or needing to sleep sitting up'
 //    ],
 //   },
 //   {
@@ -209,3 +336,4 @@
 //     chilren = showMore.lastElementChild
 //   }
 // })
+
